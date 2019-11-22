2d22333d7a90122f2c2ac7141702bbf04b5c58b9
Clemens Fritzsch <fritzsch@informatik.uni-leipzig.de>
2019-11-22 17:47:10 +0100

    [ADD] tests for SerialWriter; [ADD] stub for script to generate faulty EEPROMs

---
+++ tests/serial_writer_test.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from array import array
+
+from avocado import Test
+
+sys.path.append(
+	os.path.dirname(
+		os.path.dirname(os.path.abspath(__file__))
+	)
+)
+
+from serial_writer import SerialWriter
+
+class SerialWriterTest(Test):
+	
+	def setUp(self):
+		self.with_serial = "eeprom_serial.bin"
+		self.without_serial = "eeprom_no_serial.bin"
+		self.serial = "E86001"
+	
+	def eeprom_from_file(self, filename):
+		path = self.get_data(filename, must_exist=True)
+		with open(path, "rb") as eeprom_file:
+			eeprom = array("B", eeprom_file.read())
+		
+		return eeprom
+	
+	def generic_good_check_test(self, test_func):
+		for filename in (self.with_serial, self.without_serial):
+			eeprom = self.eeprom_from_file(filename)
+			
+			test_func(eeprom)
+		
+	
+	def test_eeprom_check_good(self):
+		self.generic_good_check_test(SerialWriter.check_eeprom)
+	
+	def test_check_eeprom_checksum_good(self):
+		self.generic_good_check_test(SerialWriter.check_eeprom_checksum)
+	
+	def test_check_eeprom_strings_good(self):
+		self.generic_good_check_test(SerialWriter.check_eeprom_strings)
+	
+	def test_set_serial_number_eeprom(self):
+		eeprom = self.eeprom_from_file(self.without_serial)
+		expected = self.eeprom_from_file(self.with_serial)
+		
+		SerialWriter.set_serial_number_eeprom(eeprom, self.serial)
+		
+		self.assertEqual(expected, eeprom, "Writing serial number had unexpected result")
+		
+		# overwrite own number -> no change expected
+		eeprom = self.eeprom_from_file(self.with_serial)
+		
+		SerialWriter.set_serial_number_eeprom(eeprom, self.serial)
+		
+		self.assertEqual(expected, eeprom, "Writing serial number had unexpected result")
+	
+	def test_serial_number_length(self):
+		original = self.eeprom_from_file(self.without_serial)
+		start = original[0x10] + original[0x11]
+		limit = 0xf6
+		
+		for length in range(1, (limit-start)//2):
+			eeprom = original[:]
+			serial_number = "f"*length
+			SerialWriter.set_serial_number_eeprom(eeprom, serial_number)
+		
+		for length in range((limit-start)//2, (0x100-start)//2):
+			print("l={}".format(length))
+			eeprom = original[:]
+			serial_number = "f"*length
+			with self.assertRaises(AssertionError):
+				SerialWriter.set_serial_number_eeprom(eeprom, serial_number)
+			
---
+++ tests/serial_writer_test.py.data/faulty_eeprom_gen.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from array import array
+
+sys.path.append(
+	os.path.dirname(
+		os.path.dirname(
+			os.path.dirname(os.path.abspath(__file__))
+		)
+	)
+)
+
+from serial_writer import SerialWriter
+
+def store_variant(eeprom, filename, variant):
+	mainname, extension = os.path.splitext(filename)
+	new_filename = "{}.{}{}".format(mainname, variant, extension)
+	
+	with open(new_filename, "wb") as out_file:
+		out_file.write(eeprom)
+	
+	return new_filename
+
+def load_eeprom(filename):
+	with open(filename, "rb") as in_file:
+		eeprom = array("B", in_file.read())
+	
+	return eeprom
+
+def fix_checksum(eeprom):
+	checksum = SerialWriter.eeprom_checksum(eeprom)
+	eeprom[-2] == (checksum & 0xff)
+	eeprom[-1] == (checksum >> 8)
+
+def add_case(case_dict, eeprom, filename, variant, faulty):
+	"""
+	faulty = triple of boolean,
+	True iff check_eeprom, check_eeprom_checksum respectively check_eeprom_strings throws an AssertionError
+	"""
+	case_name = store_variant(eeprom, filename, variant)
+	assert case_name not in case_dict, "Two test cases named '{}'".format(case_name)
+	
+	case_dict[case_name] = faulty
+
+if __name__ == "__main__":
+	serial_filename = "eeprom_serial.bin"
+	no_serial_filename = "eeprom_no_serial.bin"
+	"""
+	#set_serial_eeprom with too long serial
+	#manufaturerer, product and serial number
+		#start before string area
+		#end after string area
+		#inconsistent length
+		#wrong type
+		#unexpected start point (gaps)
+	"""
+	
+	case_dict = {}
+	for filename in (serial_filename, no_serial_filename):
+		original = load_eeprom(filename)
+		
+		# wrong EEPROM size
+		eeprom = original[:-2]
+		fix_checksum(eeprom)
+		add_case(case_dict, eeprom, filename, "too_short", (True, False, False))
+		
+		eeprom = original[:]
+		eeprom.extend((0x00, 0x00))
+		fix_checksum(eeprom)
+		add_case(case_dict, eeprom, filename, "too_long", (True, False, False))
+		
+		#wrong checksum high
+		eeprom = original[:]
+		eeprom[-2] += 1
+		add_case(case_dict, eeprom, filename, "checksum_high", (True, True, False))
+		
+		#wrong checksum
+		eeprom[-1] -= 1
+		add_case(case_dict, eeprom, filename, "checksum", (True, True, False))
+		
+		#wrong checksum low
+		eeprom[-2] = original[-2]
+		add_case(case_dict, eeprom, filename, "checksum_low", (True, True, False))
+		
+		#checksum zero
+		eeprom[-2] = 0x00
+		eeprom[-1] = 0x00
+		add_case(case_dict, eeprom, filename, "checksum_zero", (True, True, False))
+		
+		#checksum all 1
+		eeprom[-2] = 0xff
+		eeprom[-1] = 0xff
+		add_case(case_dict, eeprom, filename, "checksum_one", (True, True, False))
+	
+	serial_eeprom = load_eeprom(serial_filename)
+	no_serial_eeprom = load_eeprom(no_serial_filename)
+	
+	#serial_number flag not set but offset and/or length
+	eeprom = no_serial_eeprom[:]
+	eeprom[0x12] = serial_eeprom[0x12]
+	eeprom[0x13] = serial_eeprom[0x13]
+	fix_checksum(eeprom)
+	add_case(case_dict, eeprom, no_serial_filename, "sn_flag", (True, False, True))
+	
+	# gap in front of serial number
+	eeprom = serial_eeprom[:]
+	offset = serial_eeprom[0x12]
+	length = serial_eeprom[0x13]
+	eeprom[offset] = 0x00
+	eeprom[offset+1] = 0x00
+	for addr in range(offset, offset+length+4):
+		eeprom[addr+2] = serial_eeprom[addr]
+	fix_checksum(eeprom)
+	add_case(case_dict, eeprom, serial_filename, "sn_gap", (True, False, True))
+	
+	
+	print(case_dict)
+
